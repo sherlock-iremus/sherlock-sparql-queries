81f4685d8f536199660b14803b9e06df9d4a76b4
Thomas Bottini <thomas.bottini@cnrs.fr>
2022-07-11 18:21:18 +0200

    <0001f995>

--- src/queries/p127.js
+++ src/queries/p127.js
@@ -1,5 +1,3 @@
-import { sparqlEndpoint } from "./sparql";
-
 export const q = (resource) => `
 PREFIX crm: <http://www.cidoc-crm.org/cidoc-crm/>
 
@@ -21,12 +19,3 @@ export const format_ancestors_label = (results) => {
 
   return s;
 };
-
-const test_query = q(
-  `http://data-iremus.huma-num.fr/id/d4b92b44-3305-44c7-87a9-a56bfd5539ec`
-);
-
-// console.log(test_query);
-// console.log(format_ancestors_label());
-
-(async () => console.log(await sparqlEndpoint(test_query)))();
