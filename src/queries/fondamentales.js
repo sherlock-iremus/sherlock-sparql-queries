a4646ea53e09443017c7cbc36a84021768c2c479
AntoineLbrn <antoinelmh@hotmail.fr>
2022-10-14 13:34:12 +0200

    [feat] display fondamentales order by measure number

---
+++ src/queries/fondamentales.js
@@ -0,0 +1,33 @@
+import { SHERLOCK_TYPE } from "../common/rdf.js";
+
+/**
+ * @param {string} resource Iri of the score
+ * @returns A formatted and executable sparql query
+ */
+export const fondamentales = (resource) => `
+
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX crm: <http://www.cidoc-crm.org/cidoc-crm/>
+PREFIX dcterms: <http://purl.org/dc/terms/>
+PREFIX sherlock: <http://data-iremus.huma-num.fr/ns/sherlock#>
+PREFIX sherlockmei: <http://data-iremus.huma-num.fr/ns/sherlockmei#>
+
+SELECT DISTINCT ?annotation ?verticality ?note_label ?measure_number
+WHERE {
+  GRAPH ?g {
+    ?verticality sherlock:has_document_context <${resource}>.
+    ?verticality crm:P2_has_type <${SHERLOCK_TYPE.verticality}>. #MEI score offset
+    ?note_in_score sherlockmei:contains_beat ?verticality.
+    ?note_in_score sherlockmei:measure_number ?measure_number.
+    GRAPH ?sherlock_graph {
+        ?annotation crm:P140_assigned_attribute_to ?verticality.
+        ?annotation crm:P177_assigned_property_of_type <${SHERLOCK_TYPE.fondamentaleIdentification}>. #Identification de fondamentale
+        ?annotation crm:P141_assigned ?note.
+        GRAPH ?sherlock_data_graph {
+            ?note crm:P1_is_identified_by ?note_label
+        }
+    }
+  }
+}
+ORDER BY ?measure_number
+`;
