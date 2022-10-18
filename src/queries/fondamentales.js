349bd1186643dbfd0d089bb347cac418ed2c92cb
AntoineLbrn <antoinelmh@hotmail.fr>
2022-10-18 16:02:36 +0200

    [feat]: fondamentales query show score iri

--- src/queries/fondamentales.js
+++ src/queries/fondamentales.js
@@ -1,10 +1,10 @@
 import { SHERLOCK_TYPE } from "../common/rdf.js";
 
 /**
- * @param {string} resource Iri of the score
+ * @param {string} score Iri of the score
  * @returns A formatted and executable sparql query
  */
-export const fondamentales = (resource) => `
+export const fondamentales = (score) => `
 
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX crm: <http://www.cidoc-crm.org/cidoc-crm/>
@@ -12,10 +12,12 @@ PREFIX dcterms: <http://purl.org/dc/terms/>
 PREFIX sherlock: <http://data-iremus.huma-num.fr/ns/sherlock#>
 PREFIX sherlockmei: <http://data-iremus.huma-num.fr/ns/sherlockmei#>
 
-SELECT DISTINCT ?annotation ?verticality ?note_label ?measure_number
+SELECT DISTINCT ?score ?annotation ?verticality ?note_label ?measure_number 
 WHERE {
   GRAPH ?g {
-    ?verticality sherlock:has_document_context <${resource}>.
+    ${bindScore(score)}
+    ?score crm:P2_has_type <${SHERLOCK_TYPE.score}>. #Score type
+    ?verticality sherlock:has_document_context ?score.
     ?verticality crm:P2_has_type <${SHERLOCK_TYPE.verticality}>. #MEI score offset
     ?note_in_score sherlockmei:contains_beat ?verticality.
     ?note_in_score sherlockmei:measure_number ?measure_number.
@@ -29,5 +31,9 @@ WHERE {
     }
   }
 }
-ORDER BY ?measure_number
+ORDER BY ?score ?measure_number
 `;
+
+const bindScore = (score) => {
+  return score ? `BIND (<${score}> as ?score).` : ''
+}
