56849ac598f3b371b6b9cdfa2dbbcc9f459f43c0
AntoineLbrn <antoinelmh@hotmail.fr>
2022-11-18 16:47:04 +0100

    [fix] http-> https

--- src/common/sparql.js
+++ src/common/sparql.js
@@ -1,5 +1,5 @@
 export const sparqlEndpoint = async (query) => {
-  let res = await fetch(`http://data-iremus.huma-num.fr/sparql`, {
+  let res = await fetch(`https://data-iremus.huma-num.fr/sparql`, {
     method: "POST",
     headers: {
       "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
