a6b761564d4477e76fc17e8f7e6aa9055fbc2f1a
Félix <felix.poullet-pages@cnrs.fr>
2024-03-27 18:21:21 +0100

    get corpus manifestations

--- lib/corpus.ts
+++ lib/corpus.ts
@@ -13,3 +13,13 @@ WHERE {
   ?corpus sherlock:has_member ?manifestation .
 }
 GROUP BY ?corpus`;
+
+export const getCorpusManifestations = (corpusUri: string) => `
+PREFIX crm: <http://www.cidoc-crm.org/cidoc-crm/>
+PREFIX sherlock: <http://data-iremus.huma-num.fr/ns/sherlock#>
+SELECT ?url FROM <http://data-iremus.huma-num.fr/graph/tonalities-projects-and-corpuses>
+WHERE {
+  <${corpusUri}> sherlock:has_member ?manifestation .
+  ?manifestation crm:P1_is_identified_by ?identifier .
+  ?identifier crm:P190_has_symbolic_content ?url .
+}`;
