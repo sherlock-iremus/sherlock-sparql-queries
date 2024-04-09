edc1d58904647d78542d5e0d073297404b0e2847
Félix <felix.poullet-pages@cnrs.fr>
2024-04-09 17:30:03 +0200

    update queries with id param

--- lib/projects.ts
+++ lib/projects.ts
@@ -2,14 +2,16 @@ export const getProjectsByCreator = (creatorUri: string) => `
 PREFIX crm: <http://www.cidoc-crm.org/cidoc-crm/>
 PREFIX iremus: <http://data-iremus.huma-num.fr/id/>
 PREFIX sherlock: <http://data-iremus.huma-num.fr/ns/sherlock#>
-SELECT ?project (COUNT(?annotation) AS ?annotations) (SAMPLE(?projectLabel) AS ?label) (SAMPLE(?projectType) AS ?type)
+SELECT ?id (COUNT(?annotation) AS ?annotations) (SAMPLE(?projectLabel) AS ?label) (SAMPLE(?projectType) AS ?type)
 FROM <http://data-iremus.huma-num.fr/graph/sherlock>
 WHERE {
-    ?project crm:P2_has_type iremus:21816195-6708-4bbd-a758-ee354bb84900 .
-    ?project crm:P14_carried_out_by <${creatorUri}> .
-    ?project crm:P1_is_identified_by ?projectLabel .
-    ?project sherlock:has_privacy_type ?projectType .
-    ?project crm:P9_consists_of ?annotation .
-    ?annotation sherlock:has_document_context ?score .
+    ?id crm:P2_has_type iremus:21816195-6708-4bbd-a758-ee354bb84900 .
+    ?id crm:P14_carried_out_by <${creatorUri}> .
+    ?id crm:P1_is_identified_by ?projectLabel .
+    ?id sherlock:has_privacy_type ?projectType .
+    OPTIONAL {
+        ?id crm:P9_consists_of ?annotation .
+        ?annotation sherlock:has_document_context ?score .
+    }
 }
-GROUP BY ?project`;
+GROUP BY ?id`;
